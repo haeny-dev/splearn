ffa84cdb7a8c45dc84d5897952080002c7c96211
haeny-dev <wjdeoaks92@gmail.com>
2026-01-24 15:53:07 +0900

    feat: API 개발 및 테스트 작성 및 ArchUnit 테스트 추가

---
+++ src/test/java/tobyspring/splearn/AssertThatUtils.java
@@ -0,0 +1,19 @@
+package tobyspring.splearn;
+
+import org.assertj.core.api.AssertProvider;
+import org.springframework.test.json.JsonPathValueAssert;
+import tobyspring.splearn.domain.member.MemberRegisterRequest;
+
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AssertThatUtils {
+    public static Consumer<AssertProvider<JsonPathValueAssert>> notNull() {
+        return value -> assertThat(value).isNotNull();
+    }
+
+    public static Consumer<AssertProvider<JsonPathValueAssert>> equalTo(String expected) {
+        return value -> assertThat(value).isEqualTo(expected);
+    }
+}
